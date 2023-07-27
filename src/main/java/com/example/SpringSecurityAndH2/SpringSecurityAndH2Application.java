636b8784d026ccbbaa24234441be9c30cfb760cc
SakiShiomura <shiomura@trecodea.com>
2023-07-27 16:05:15 +0900

    Initial commit

---
+++ src/main/java/com/example/SpringSecurityAndH2/SpringSecurityAndH2Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringSecurityAndH2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAndH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAndH2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringSecurityAndH2/SpringSecurityAndH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringSecurityAndH2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAndH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
